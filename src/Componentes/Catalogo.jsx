cdddac4b37b3e26ffd720ef53cb1e059b02caa8c
Laraana <cecylar14@gmail.com>
2025-01-26 01:21:50 -0600

    Aplicación funcional | Pendiente el comentado del codigo y optimización de claves

--- src/Componentes/Catalogo.jsx
+++ src/Componentes/Catalogo.jsx
@@ -1,71 +1,93 @@
-import { useEffect, useState } from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TextField, TablePagination, Dialog, DialogActions, DialogContent, DialogTitle, Button } from '@mui/material';
-import { Select, MenuItem, FormControl, InputLabel } from '@mui/material';
-import axios from 'axios';
+import { useEffect, useState } from 'react'; //Para los estados de React
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TextField, TablePagination, Dialog, DialogActions, DialogContent, DialogTitle, Button } from '@mui/material'; //Libreria para los diseños de las tablas
+import { Select, MenuItem, FormControl, InputLabel } from '@mui/material'; //Libreria para cada elemento de los formularios
+import axios from 'axios'; //Libreria para interactuar con el backend
 
 const Catalogo = () => {
-  const [dataVista, setDataVista] = useState([]);
-  const [dataOperador, setDataOperador] = useState([]);
-  const [dataMaquinas, setDataMaquinas] = useState([]);
 
+  /**########### PARA EL LLENADO DE LAS TABLAS ############## */
+  const [dataVista, setDataVista] = useState([]); //Obtener los datos de la vista
+  const [dataOperador, setDataOperador] = useState([]); //Obtener los datos de los operadores
+  const [dataMaquinas, setDataMaquinas] = useState([]); //Obtener los datos de las maquinas
+  /**######################################################## */
+
+  /**########### PARA GENERAR LOS FILTROS ############## */
   const [filteredVista, setFilteredVista] = useState([]);
   const [filteredOperador, setFilteredOperador] = useState([]);
   const [filteredMaquinas, setFilteredMaquinas] = useState([]);
+  /**######################################################## */
 
+  /**########### PARA LAS BUSQUEDAS ############## */
   const [searchQuery, setSearchQuery] = useState('');
-  const [selectedComponent, setSelectedComponent] = useState('Vista');
+  /**######################################################## */
 
+  /**########### PARA DETERMINAR QUE PESTAÑA DEL CATALOGO ESTAMOS SELECCIONANDO ############## */
+  const [selectedComponent, setSelectedComponent] = useState('Vista');
+  /**######################################################## */
+
+  /**########### PARA EL PAGINADO DE LA TABLA POR PESTAÑA ############## */
   const [pageVista, setPageVista] = useState(0);
   const [pageOperador, setPageOperador] = useState(0);
   const [pageMaquinas, setPageMaquinas] = useState(0);
+  /**####################################################### */
 
+  /**########### PARA LA CANTIDAD DE FILAS QUE SE MOSTRARAN EN LAS TABLAS ############## */
   const [rowsPerPage, setRowsPerPage] = useState(5);
-  const [statusModule, setStatusModule] = useState(5);
+  /**####################################################### */
 
-  // Estado del Modal
+  /**########### PARA GENERAR LAS ACCIONES DE LOS BOTONES ############## */
+  const [statusModule, setStatusModule] = useState(0);
+  /**####################################################### */
+
+  /**########### PARA EL ESTADO DEL MODAL Y SU INFORMACIÓN ############## */
   const [openModal, setOpenModal] = useState(false);
-  const [modalData, setModalData] = useState(null);  // o un objeto vacío si ya tienes datos por defecto
+  const [modalData, setModalData] = useState({});
+  /**####################################################### */
 
-  // Función para abrir el modal con los detalles del item seleccionado
-  const openDetailsModal = (item, status = 0) => {
-    setStatusModule == status
-    // Si 'item' es null, asigna valores vacíos a modalData
-    if (item === null) {
-      if (selectedComponent === 'Operador') {
-        setModalData({
-          Op_Nombre: '',
-          Op_Area: '',
-        });
-      } else if (selectedComponent === 'Maquinas') {
-        setModalData({
-          Maq_Descripcion: '',
-        });
-      } else if (selectedComponent === 'Vista') {
-        if (status === 0) {
-          // Si status es 0, muestra un mensaje de confirmación
+  // Función para abrir el modal con los detalles del item seleccionado y status seleccionado
+  const openDetailsModal = (item, status) => {
+    setStatusModule(status);  // Asignamos el valor del status para usarlo fuera en la botonera de cada modal
+
+    // para manejar de manera exclusiva los eventos de deshabilitar y habilitar cada registro de asociación de maquinas
+    if ((status === 0 || status === 2) && selectedComponent === 'Vista') {
+      const titulo = status === 0 ? 'desasociar' : 'asociar nuevamente';
+
+      setModalData({
+        confirmationMessage: `¿Estás seguro de querer ${titulo} el equipo al operador?`,
+        ...item, //Controlamos respetando la estructura de la información seleccionadaS
+      });
+    } else {
+      // Si item es null, asegúrate de dar valores predeterminados
+      if (item === null) {
+        if (selectedComponent === 'Operador') {
           setModalData({
-            confirmationMessage: '¿Estás seguro de que deseas continuar?', // Ajusta el mensaje según necesites
+            Op_Nombre: '',
+            Op_Area: '',
           });
-        } else {
-          // Aquí asumes que quieres llenar el modal con dos selects
+        } else if (selectedComponent === 'Maquinas') {
           setModalData({
-            SelectOption1: '', // Asumiendo que el modal tendrá dos selects
+            Maq_Descripcion: '',
+          });
+        } else if (selectedComponent === 'Vista') {
+          setModalData({
+            SelectOption1: '',
             SelectOption2: '',
           });
         }
+      } else {
+        // Si item no es null, asignamos sus valores
+        setModalData(item);
       }
-    } else {
-      // Si 'item' no es null, asigna los valores del item
-      setModalData(item);
     }
 
-    setOpenModal(true);
+    setOpenModal(true); // Aquí abrimos el modal
   };
 
   // Función para cerrar el modal
   const closeModal = () => {
     setOpenModal(false);
     setModalData(null);
+    setStatusModule(0);
   };
 
   // Función para filtrar los datos
@@ -84,7 +106,7 @@ const Catalogo = () => {
 
   const handleChangeRowsPerPage = (event) => {
     setRowsPerPage(parseInt(event.target.value, 10));
-    setPageVista(0); // Reset page to 0 when changing rows per page
+    setPageVista(0);
     setPageOperador(0);
     setPageMaquinas(0);
   };
@@ -97,21 +119,43 @@ const Catalogo = () => {
     }));
   };
 
+  useEffect(() => {
+    // Cuando los datos de la vista cambien, actualizamos los datos filtrados.
+    setFilteredVista(dataVista);
+  }, [dataVista]);
+
+  useEffect(() => {
+    setFilteredOperador(dataOperador);
+  }, [dataOperador]);
+
+  useEffect(() => {
+    setFilteredMaquinas(dataMaquinas);
+  }, [dataMaquinas]);
+
   const handleEdit = () => {
-    if (selectedComponent === 'Vista') {
-      console.log("El componente seleccionado es Vista");
-      axios
-        .put('http://localhost/api_catalogos/api_vista.php', modalData)
-        .then((response) => {
-        })
-        .catch((error) => {
-          console.error('Error al actualizar los datos:', error);
-        });
-    } else if (selectedComponent === 'Operador') {
-      console.log("El componente seleccionado es Operador");
+    if (selectedComponent === 'Operador') {
       axios
         .put('http://localhost/api_catalogos/api_op.php', modalData)
         .then((response) => {
+          // Volver a obtener los datos para actualizar las tablas
+          axios.get('http://localhost/api_catalogos/api_op.php')
+            .then((response) => {
+              setDataOperador(response.data);
+              setFilteredOperador(response.data); // Actualizar datos filtrados
+            })
+            .catch((error) => {
+              console.error('Error al obtener los datos:', error);
+            });
+
+          // Volver a obtener los datos para actualizar las tablas
+          axios.get('http://localhost/api_catalogos/api_vista.php')
+            .then((response) => {
+              setDataVista(response.data);
+              setFilteredVista(response.data); // Actualizar datos filtrados
+            })
+            .catch((error) => {
+              console.error('Error al obtener los datos:', error);
+            });
         })
         .catch((error) => {
           console.error('Error al actualizar los datos:', error);
@@ -120,22 +164,57 @@ const Catalogo = () => {
       axios
         .put('http://localhost/api_catalogos/api_maq.php', modalData)
         .then((response) => {
+          axios.get('http://localhost/api_catalogos/api_maq.php')
+            .then((response) => {
+              setDataMaquinas(response.data);
+              setFilteredMaquinas(response.data); // Actualizar datos filtrados
+            })
+            .catch((error) => {
+              console.error('Error al obtener los datos:', error);
+            });
+
+          axios.get('http://localhost/api_catalogos/api_vista.php')
+            .then((response) => {
+              setDataVista(response.data);
+              setFilteredVista(response.data); // Actualizar datos filtrados
+            })
+            .catch((error) => {
+              console.error('Error al obtener los datos:', error);
+            });
         })
         .catch((error) => {
           console.error('Error al actualizar los datos:', error);
         });
     }
 
-    // Realiza cualquier acción que necesites después de actualizar, como cerrar el modal
     closeModal();
-
   };
 
   const handleInsert = () => {
+    const currentPage = selectedComponent === 'Vista' ? pageVista : selectedComponent === 'Operador' ? pageOperador : pageMaquinas;
     if (selectedComponent === 'Operador') {
       axios
         .post('http://localhost/api_catalogos/api_op.php', modalData)
         .then((response) => {
+          // Volver a obtener los datos para actualizar las tablas
+          axios.get('http://localhost/api_catalogos/api_op.php')
+            .then((response) => {
+              setDataOperador(response.data);
+              setFilteredOperador(response.data); // Actualizar datos filtrados
+            })
+            .catch((error) => {
+              console.error('Error al obtener los datos:', error);
+            });
+
+          // Volver a obtener los datos para actualizar las tablas
+          axios.get('http://localhost/api_catalogos/api_vista.php')
+            .then((response) => {
+              setDataVista(response.data);
+              setFilteredVista(response.data); // Actualizar datos filtrados
+            })
+            .catch((error) => {
+              console.error('Error al obtener los datos:', error);
+            });
         })
         .catch((error) => {
           console.error('Error al agregar los datos:', error);
@@ -144,6 +223,23 @@ const Catalogo = () => {
       axios
         .post('http://localhost/api_catalogos/api_maq.php', modalData)
         .then((response) => {
+          axios.get('http://localhost/api_catalogos/api_maq.php')
+            .then((response) => {
+              setDataMaquinas(response.data);
+              setFilteredMaquinas(response.data); // Actualizar datos filtrados
+            })
+            .catch((error) => {
+              console.error('Error al obtener los datos:', error);
+            });
+
+          axios.get('http://localhost/api_catalogos/api_vista.php')
+            .then((response) => {
+              setDataVista(response.data);
+              setFilteredVista(response.data); // Actualizar datos filtrados
+            })
+            .catch((error) => {
+              console.error('Error al obtener los datos:', error);
+            });
         })
         .catch((error) => {
           console.error('Error al agregar los datos:', error);
@@ -152,14 +248,55 @@ const Catalogo = () => {
       axios
         .post('http://localhost/api_catalogos/api_vista.php', modalData)
         .then((response) => {
+          // Volver a obtener los datos para actualizar las tablas
+          axios.get('http://localhost/api_catalogos/api_vista.php')
+            .then((response) => {
+              setDataVista(response.data);
+              setFilteredVista(response.data); // Actualizar datos filtrados
+            })
+            .catch((error) => {
+              console.error('Error al obtener los datos:', error);
+            });
         })
         .catch((error) => {
           console.error('Error al agregar los datos:', error);
         });
     }
 
+    setPageVista(currentPage); // O el componente seleccionado correspondiente
     closeModal();
+  };
 
+  const handleConfirmation = () => {
+    let actionData = { ...modalData };
+    if (selectedComponent === 'Vista') {
+      if (statusModule === 0) {
+        actionData.action = 'desasociar';
+        console.log("Desasociar ", modalData.Clave_Vista);
+      }
+      else if (statusModule === 2) {
+        actionData.action = 'asociar';
+        console.log("Asociar", modalData.Clave_Vista);
+      }
+
+      axios
+        .put('http://localhost/api_catalogos/api_vista.php', actionData)
+        .then((response) => {
+          // Volver a obtener los datos para actualizar las tablas
+          axios.get('http://localhost/api_catalogos/api_vista.php')
+            .then((response) => {
+              setDataVista(response.data);
+              setFilteredVista(response.data); // Actualizar datos filtrados
+            })
+            .catch((error) => {
+              console.error('Error al obtener los datos:', error);
+            });
+        })
+        .catch((error) => {
+          console.error('Error al actualizar los datos:', error);
+        });
+    }
+    closeModal();
   };
 
   useEffect(() => {
@@ -173,7 +310,6 @@ const Catalogo = () => {
         setDataVista(responseVista.data);
         setDataOperador(responseOperador.data);
         setDataMaquinas(responseMaquinas.data);
-
         // Inicialmente, todos los datos filtrados son iguales a los datos originales
         setFilteredVista(responseVista.data);
         setFilteredOperador(responseOperador.data);
@@ -237,12 +373,12 @@ const Catalogo = () => {
                   <TableCell>{item.Clave_Maq}</TableCell>
                   <TableCell>{item.Maq_Descripcion}</TableCell>
                   <TableCell>
-                    {item.Vista_Activo === 1 ? (
-                      <Button onClick={() => openDetailsModal(item)}>✔️</Button>
+                    {/* Asegúrate de que Vista_Activo es un número */}
+                    {parseInt(item.Vista_Activo, 10) === 1 ? (
+                      <Button onClick={() => openDetailsModal(item, 0)}>X</Button>
                     ) : (
-                      <Button onClick={() => openDetailsModal(item)}>X</Button>
+                      <Button onClick={() => openDetailsModal(item, 2)}>✔️</Button>
                     )}
-                    <Button onClick={() => openDetailsModal(item, 1)}>Editar</Button>
                   </TableCell>
                 </TableRow>
               ))}
@@ -381,7 +517,12 @@ const Catalogo = () => {
         <DialogContent>
           {modalData && (
             <>
-              {selectedComponent === 'Vista' && (
+              {modalData.confirmationMessage && (
+                // Mostrar el mensaje de confirmación
+                <div>{modalData.confirmationMessage}</div>
+              )}
+
+              {selectedComponent === 'Vista' && !modalData.confirmationMessage && (
                 <>
                   <FormControl fullWidth margin="normal">
                     <InputLabel>Seleccione Operador</InputLabel>
@@ -445,20 +586,51 @@ const Catalogo = () => {
                   />
                 </>
               )}
-
             </>
           )}
         </DialogContent>
         <DialogActions>
-          {/* Aseguramos que 'modalData.status' tenga un valor antes de usarlo */}+
-          <Button onClick={closeModal} color="error">Desasociar</Button>
-          <Button onClick={handleEdit} color="success">Editar</Button>
-          <Button onClick={handleInsert} color="success">Insertar</Button>
-          <Button onClick={closeModal} color="primary">Cerrar</Button>
+          {modalData?.confirmationMessage && (
+            <>
+              <Button
+                onClick={handleConfirmation} // Llama a una función para confirmar
+                color="error"
+              >
+                Confirmar
+              </Button>
+              <Button onClick={closeModal} color="primary">
+                Cancelar
+              </Button>
+            </>
+          )}
+
+          {statusModule === 2 && !modalData?.confirmationMessage && (
+            <>
+              <Button onClick={closeModal} color="warning">Asociar</Button>
+              <Button onClick={closeModal} color="primary">Cerrar</Button>
+            </>
+          )}
+          {statusModule === 0 && !modalData?.confirmationMessage && (
+            <>
+              <Button onClick={closeModal} color="error">Desasociar</Button>
+              <Button onClick={closeModal} color="primary">Cerrar</Button>
+            </>
+          )}
+          {statusModule === 1 && !modalData?.confirmationMessage && (
+            <>
+              <Button onClick={handleEdit} color="success">Editar</Button>
+              <Button onClick={closeModal} color="primary">Cerrar</Button>
+            </>
+          )}
+          {statusModule !== 2 && statusModule !== 0 && statusModule !== 1 && !modalData?.confirmationMessage && (
+            <>
+              <Button onClick={handleInsert} color="success">Insertar</Button>
+              <Button onClick={closeModal} color="primary">Cerrar</Button>
+            </>
+          )}
         </DialogActions>
       </Dialog>
     </>
   );
 };
-
 export default Catalogo;
